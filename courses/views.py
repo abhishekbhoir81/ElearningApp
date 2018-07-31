a648e8bbfb819e8037957296cfd4e2eccd58a44a
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-07-31 10:36:02 +0530

    Reordering Modules and contents Using mixins from django-braces

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -171,3 +172,23 @@ class ManageCourseListView(ListView):
     def get_queryset(self):
         qs = super(ManageCourseListView, self).get_queryset()
         return qs.filter(owner=self.request.user)
+
+
+class ModuleOrderView(CsrfExemptMixin,
+                      JsonRequestResponseMixin,
+                      View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                   course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin,
+                       View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                       module__course__owner=request.user) \
+                       .update(order=order)
+        return self.render_json_response({'saved': 'OK'})        
