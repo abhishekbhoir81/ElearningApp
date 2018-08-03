9b04660c91fbdcea1371ca52c8db6df5e3664cb5
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-08-03 21:13:06 +0530

    Displaying Courses

--- courses/views.py
+++ courses/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, \
                                       DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, \
                                        PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View                                                                             
-from .models import Course
 from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
-from .models import Module, Content
+from django.db.models import Count
+from .models import Module, Content, Course, Subject 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class OwnerMixin(object):
@@ -191,4 +192,25 @@ class ContentOrderView(CsrfExemptMixin,
             Content.objects.filter(id=id,
                        module__course__owner=request.user) \
                        .update(order=order)
-        return self.render_json_response({'saved': 'OK'})        
+        return self.render_json_response({'saved': 'OK'})   
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+                       total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+                        total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})             
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
